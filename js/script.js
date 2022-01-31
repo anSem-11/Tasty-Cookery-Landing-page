db137922e83acd11b7b991a5b0936321f473bb16
Anna Semenova <93093855+anSem-11@users.noreply.github.com>
2022-01-31 21:38:16 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+$(function() {
+  var $window = $(window);
+  var $slider = $('.slider');
+  var hasInitSlider = false;
+  
+  function initSlider() {
+    if ($window.width() <= 425 && !hasInitSlider) {
+      $slider.slick({
+        speed:500,
+        autoplay:true,
+        arrows:false,
+        dots: true,
+      });
+      hasInitSlider = true;
+    } else if ($window.width() > 425 && hasInitSlider) {
+      $slider.slick('unslick');
+      hasInitSlider = false;
+    }
+  }
+  
+  initSlider();
+  
+  $window.on('resize', function() {
+    initSlider();
+  });
+});
+
+// // Validation form
+function validation() {
+  const signform = document.getElementById("signform");
+  const email = document.getElementById("email").value;
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+  } 
+  else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+  }
+}
+
+// Script для отправки формы (ajax)
+
+$("#signform").submit(function(e){
+  e.preventDefault();
+  $.ajax({
+      type: "POST",
+      url: "wdh_send_form.php",
+      data: $("#signform").serialize(),
+      success: function(data) {
+          alert('письмо').html(data);
+      }
+  });
+});
+
+
+
+
+
